10fe687f4ac58ef0c4943b403ad93674390ddb11
Nataliia Boiko <nataliboiko.ua@gmail.com>
2024-03-18 17:38:14 +0000

    changed paths

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import Navigation from './components/Navigation.js';
-import Banner from './components/Banner.js';
-import Information from './components/Information.js';
-import Projects from './components/Projects.js';
-import Contacts from './components/Contacts.js';
+
+import React, { useEffect } from 'react';
+import './App.css';
+import Navigation from './components/Navigation';
+import Banner from './components/Banner';
+import Information from './components/Information';
+import Projects from './components/Projects';
+import Contacts from './components/Contacts';
 
 function App() {
+  useEffect(() => {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+          e.preventDefault();
+
+          const target = document.querySelector(this.getAttribute('href'));
+
+          if (target) {
+              target.scrollIntoView({
+                  behavior: 'smooth'
+              });
+          }
+      });
+    });
+  }, []);
+
   return (
-    <div>
+    <div className="App">
       <Navigation />
-      <Banner />
-      <Information />
-      <Projects/>
+      <div id="home">
+        <Banner />
+      </div>
+      <div id="skills">
+        <Information />
+      </div>
+      <div id="projects">
+        <Projects/>
+      </div>
       <Contacts/>
     </div>
   );
